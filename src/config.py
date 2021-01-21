58625b139432815026a8f1b96cf83be5d423a4d5
debajyoti <debajyoti94@gmail.com>
2021-01-21 16:43:31 +0530

    Added configuration variables for training KNN model.

---
+++ src/config.py
@@ -0,0 +1,16 @@
+''' In this code we define the configuration variables
+that are needed throughout this project'''
+
+# for dataset
+ORIGINAL_RAW_DATASET = '../input/diabetes.csv'
+
+RAW_TRAIN_DATASET = '../raw_train_set.pickle'
+RAW_TEST_DATASET = '../input/raw_test_set.pickle'
+
+CLEAN_TRAIN_DATASET = '../input/clean_train_set.pickle'
+CLEAN_TEST_DATASET = '../input/clean_test_set.pickle'
+
+# for model training purposes
+DISTANCE_METRIC = 'cosine'
+NUM_FOLDS = 5
+BASELINE_MODEL = '../models/KNN_BASELINE_'
