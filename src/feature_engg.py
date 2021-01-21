4148bb82e99a66f40bcd543c00b4af9caffd80d1
debajyoti <debajyoti94@gmail.com>
2021-01-21 17:23:00 +0530

    Added functionality for dumping and loading files.

--- src/feature_engg.py
+++ src/feature_engg.py
@@ -2,7 +2,11 @@
 
 
 # import modules here
+import config
+import pickle
 
+from sklearn import preprocessing as preproc
+import seaborn as sns
 
 # abc class here
 
@@ -11,3 +15,23 @@
 
 
 # dump and load object class here
+class DumpLoadFile:
+
+    def load_file(self, filename):
+        '''
+        For loading the pickled files
+        :param filename: the file that you want to load
+        :return: the loaded file
+        '''
+        with open(filename, 'rb') as pickle_handle:
+            return pickle.load(pickle_handle)
+
+    def dump_file(self, file, filename):
+        '''
+        Pickle a file
+        :param file: file that you want to pickle
+        :param filename: filename for the pickled file
+        :return: nothing
+        '''
+        with open(filename, 'wb') as pickle_handle:
+            pickle.dump(file, pickle_handle)
