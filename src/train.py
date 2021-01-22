08d38946f947ee3f4dd91c1de3b8c05ae76af89c
debajyoti <debajyoti94@gmail.com>
2021-01-22 16:49:45 +0530

    Added necessary modules and now working on adding commandline arguments.

--- src/train.py
+++ src/train.py
@@ -1,14 +1,41 @@
 '''Here we will train and test the model'''
 
 # import modules here
+import config
+import feature_engg
+import create_folds
+
+import argparse
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
 
 # train function here
+def run(dataset, fold):
+    '''
 
+    :param dataset:
+    :param fold:
+    :return:
+    '''
+    return
 
 # test function here
+def inference_stage(dataset, model):
+    '''
+
+    :param dataset:
+    :param model:
+    :return:
+    '''
+    return
 
 
 # main gateway here
+if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser()
+
+    # adding the arguments
 
 #commandline arguments here
