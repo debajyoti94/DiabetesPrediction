8d6f30a020499afd83bab124957b3d0a00128268
debajyoti <debajyoti94@gmail.com>
2021-01-22 19:47:27 +0530

    Added code for training the model.

--- src/train.py
+++ src/train.py
@@ -9,19 +9,48 @@ import argparse
 import numpy as np
 
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import precision_recall_fscore_support, \
+                            accuracy_score
 
 # train function here
-def run(X_train, y_train,
+def run(dataset,
         k_neighbours, fold):
     '''
 
-    :param X_train:
-    :param y_train:
+    :param dataset:
     :param k_neighbours:
     :param fold:
     :return:
     '''
+    train_set = dataset[dataset.kfold != fold]
+    valid_set = dataset[dataset.kfold == fold]
+
+    # split train set to X and y
+    X_train = train_set.drop(config.OUTPUT_FEATURE, axis=1,
+                             inplace=False)
+    y_train = train_set[config.OUTPUT_FEATURE]
+
+    # split valid set to X and y
+    X_valid = valid_set.drop(config.OUTPUT_FEATURE, axis=1,
+                             inplace=False)
+    y_valid = valid_set[config.OUTPUT_FEATURE]
+
+    # instantiating the knn classifier
+    knn = KNeighborsClassifier(n_neighbors=k_neighbours,
+                               metric=config.DISTANCE_METRIC)
+
+    # fit the model
+    knn.fit(X_train, y_train)
+    # get predictions
+    preds = knn.predict(X_valid)
+
+    # getting the mean accuracy to get the optimum k value
+    train_accuracy = knn.score(X_train, y_train)
+    test_accuracy = knn.score(X_valid, y_valid)
+
+    # since we are using mean accuracy to measure the model performance
+    return train_accuracy, test_accuracy
+
 
 # test function here
 def inference_stage(dataset, model):
@@ -69,33 +98,53 @@ if __name__ == "__main__":
         raw_test_set = dl_obj.load_file(config.RAW_TEST_DATASET)
 
         # clean the datasets
-        X_clean_train_set, y_clean_train_set = fr_obj.cleaning_data(raw_train_set)
-        X_clean_test_set, y_clean_test_set = fr_obj.cleaning_data(raw_test_set)
+        clean_train_set = fr_obj.cleaning_data(raw_train_set)
+        clean_test_set = fr_obj.cleaning_data(raw_test_set)
 
         # dump the clean datasets
-        dl_obj.dump_file(config.CLEAN_TRAIN_DATASET, X_clean_train_set, y_clean_train_set)
-        dl_obj.dump_file(config.CLEAN_TEST_DATASET, X_clean_test_set, y_clean_test_set)
+        dl_obj.dump_file(config.CLEAN_TRAIN_DATASET, clean_train_set)
+        dl_obj.dump_file(config.CLEAN_TEST_DATASET, clean_test_set)
 
     elif args.train == 'skfold':
 
         # load the train dataset
-        X_train, y_train = dl_obj.load_file(config.CLEAN_TRAIN_DATASET)
-        print(X_train.shape, y_train.shape)
+        train_set = dl_obj.load_file(config.CLEAN_TRAIN_DATASET)
 
+        # the train set changing to a tuple hence doing this
+        train_set = train_set[0]
+
+        # print(type(train_set))
+        # print(train_set.shape)
+
+        skfold_obj = create_folds.SKFold()
+        train_set[config.KFOLD_COLUMN_NAME] = -1
+        train_set = skfold_obj.create_folds(train_set, config.NUM_FOLDS)
         # we need to maintain the f1 score on average
         # for all 5 folds for all k neighbours
-        f1_score_avg = []
+        train_accuracy_avg = []
+        test_accuracy_avg = []
 
         for k in range(1,51):
-            f1_score_run = []
+            train_accuracy_per_k = []
+            test_accuracy_per_k = []
             for fold_value in range(config.NUM_FOLDS):
-                f1_score_run.append(run(X_train=X_train, y_train=y_train,
-                                    k_neighbours=k, fold=fold_value))
+                train_accuracy, test_accuracy = run(dataset=train_set,
+                                    k_neighbours=k, fold=fold_value)
+
+                train_accuracy_per_k.append(train_accuracy)
+                test_accuracy_per_k.append(test_accuracy)
 
             # storing the average test score of 5 runs for each k
-            f1_score_avg.append(np.mean(f1_score_run))
+            train_accuracy_avg.append(np.mean(train_accuracy_per_k))
+            test_accuracy_avg.append(np.mean(test_accuracy_per_k))
 
+        # print(train_accuracy_avg, test_accuracy_avg)
         # dump the metric score, as we will use this as a plot
+        dl_obj.dump_file(config.TRAIN_ACCURACY_SCORE, train_accuracy_avg)
+        dl_obj.dump_file(config.TEST_ACCURACY_SCORE, test_accuracy_avg)
+
+        print("Model training complete.\n"
+              " Please refer to accuracy scores to identify the optimum k value.")
 
     elif args.test == 'inference':
         pass
