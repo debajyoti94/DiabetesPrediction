5e46c34edd349e38225c8d0cba9021b6990f0457
debajyoti <debajyoti94@gmail.com>
2021-01-22 12:20:26 +0530

    Added variable for plots.

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@
 that are needed throughout this project'''
 
 # for dataset
+OUTPUT_FEATURE = 'Outcome'
 ORIGINAL_RAW_DATASET = '../input/diabetes.csv'
 
 RAW_TRAIN_DATASET = '../raw_train_set.pickle'
@@ -9,6 +10,9 @@ RAW_TEST_DATASET = '../input/raw_test_set.pickle'
 
 CLEAN_TRAIN_DATASET = '../input/clean_train_set.pickle'
 CLEAN_TEST_DATASET = '../input/clean_test_set.pickle'
+
+# for plots
+NULL_CHECK_HEATMAP = '../plots/null_check_heatmap.png'
 
 # for model training purposes
 DISTANCE_METRIC = 'cosine'
